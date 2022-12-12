d7301388defc7c66526c0184e059290ecb87119a
快乐的老鼠宝宝 <keaitianxinmail@qq.com>
2022-12-13 00:08:50 +0800

    feat: 按照最高阶遍历

--- src/adminextract.py
+++ src/adminextract.py
@@ -5,7 +5,7 @@ from kqs.waifu import Waifu
 
 
 def show_hierarchy(relation_list: List[List[str]]) -> None:
-    hierarchy: Dict[int, List[str]] = {}
+    hierarchy: Dict[str, List[List[str]]] = {}
     for relation in relation_list:
         admin_level = relation[1]
         if hierarchy.get(admin_level) == None:
@@ -45,11 +45,46 @@ def extract_admin_relation(map: Waifu) -> List[List[str]]:
     return admin_relation
 
 
+def extract_tree(admin_relation: List[List[str]], map: Waifu) -> list:
+    admin_tree = []
+    # 是否需要手动指定根节点？
+    # 在找得到唯一的最高等级的时候可以直接用，但如果就是查非最高级开始的咋办
+    # 然后就是逐个访问了，这里建议深度优先策略
+
+    strategy_root = "auto"  # highest=指定最高级的，不行就炸，auto就是上述判断不行就要求手输，input就是手输为重
+    if strategy_root == "auto" or strategy_root == "highest":
+        highest_admin_value = 999
+        highest_admin_id = 0
+        highest_admin_count = 0
+        highest_admin = []
+        for relation in admin_relation:
+            if highest_admin_value!="" and (int(relation[1]) <= int(highest_admin_value)):
+                highest_admin_value = relation[1]
+                highest_admin_id = relation[0]
+                highest_admin_count += 1
+                highest_admin.append([highest_admin_id, highest_admin_value])
+        print(highest_admin)
+        if highest_admin_count > 1:
+            # 查找失败
+            highest_admin_id = input("请输入要查询的根关系的id")
+            print(highest_admin_id)
+        else:
+            print(highest_admin_id)
+    elif strategy_root == "input":
+        highest_admin_id = input("请输入要查询的根关系的id")
+        print(highest_admin_id)
+    else:
+        print("¿")
+
+    return admin_tree
+
+
 def main():
     map = Waifu()
     map.read(mode="file", file_path="map.osm")
     admin_relation = extract_admin_relation(map)
     show_hierarchy(admin_relation)
+    extract_tree(admin_relation,map)
 
 
 if __name__ == "__main__":
